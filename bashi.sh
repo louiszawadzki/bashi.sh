4f0bdd1e6fa3cbaf097510ef310b51b145a44cad
louiszawadzki <louisz@theodo.fr>
2017-01-09 22:35:27 +0100

    Send a file located on the server

--- bashi.sh
+++ bashi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Starts the web server
-while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo "hello"; } | nc -l 8888; done
+while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; lib/send_response.sh; } | nc -l 8888; done
---
+++ lib/send_response.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Sends the a file based located on the src/static/ folder
+cat src/static/index.html
