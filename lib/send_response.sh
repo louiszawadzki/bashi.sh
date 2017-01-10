0e9cd5c0bb9bc6650632eae88d846f50d03524b3
louiszawadzki <louisz@theodo.fr>
2017-01-10 13:35:05 +0100

    Send 404 if file is not found

--- lib/send_response.sh
+++ lib/send_response.sh
@@ -2,10 +2,15 @@
 # Imports
 . lib/get_path.sh
 
-filename=$(get_file_from_url)
+filename=src/static/$(get_file_from_url)
 
-# Send Headers
-echo -e 'HTTP/1.1 200 OK\r\n'
-
-# Send the a file based located on the src/static/ folder
-cat src/static/"$filename"
+if [ -e $filename ]
+then
+  # Send Headers
+  echo -e 'HTTP/1.1 200 OK\r\n'
+  # Send the a file based located on the src/static/ folder
+  cat $filename
+else
+  # Send 404 header
+  echo -e 'HTTP/1.1 404 Not Found\r\n'
+fi
